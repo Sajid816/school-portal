76b2a50bf95377bc439134e70e118db5d6de235f
Sajid816 <98689586+Sajid816@users.noreply.github.com>
2026-06-21 16:38:41 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,16 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  
-  apiKey: "AIzaSyBi1E_RIk0UrN6_fG7GTkzCztOgjavRdv8", 
-  authDomain: "school-database-b5981.firebaseapp.com",
-  projectId: "school-database-b5981",
-  storageBucket: "school-database-b5981.appspot.com",
-  messagingSenderId: "105403881102",
-  appId: "1:105403881102:web:644846c867207c416f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// Add this import at the top if you don't have it
-// import { initializeApp } from "firebase/app";
-
-// Add this to the absolute bottom of firebase.js
-const secondaryApp = initializeApp(firebaseConfig, "SecondaryApp");
-export const secondaryAuth = getAuth(secondaryApp);
